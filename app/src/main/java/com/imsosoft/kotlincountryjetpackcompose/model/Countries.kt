7d134ebbb976eadb492481a22ac525453677e79b
Muarem <s.muarem10@gmail.com>
2024-03-13 20:39:34 +0400

    refactored Countries

--- app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/model/Countries.kt
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/model/Countries.kt
@@ -2,4 +2,4 @@ package com.imsosoft.kotlincountryjetpackcompose.model
 
 import com.google.gson.annotations.SerializedName
 
-class Countries: ArrayList<Countries>()
+class Countries: ArrayList<CountriesItem>()
