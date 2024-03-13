fda8fa6239f7f81d5984145b3822e6e49f73b0c4
Muarem <s.muarem10@gmail.com>
2024-03-13 19:12:53 +0400

    created Constants.kt for constants

---
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.imsosoft.kotlincountryjetpackcompose.util
+
+object Constants {
+
+    const val BASE_URL = "https://raw.githubusercontent.com/MaharramSadikhli/APIs/main/"
+    const val END_POINT_COUNTRIES = "countryapi.json"
+    const val END_POINT_FLAGS = "countryapiflags.json"
+
+}
