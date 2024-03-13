64f45bab29cd897bddf25ee722a563e0a48b2c2a
Muarem <s.muarem10@gmail.com>
2024-03-13 22:26:15 +0400

    created extension fun

---
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/util/ExtensionFunc.kt
@@ -0,0 +1,20 @@
+package com.imsosoft.kotlincountryjetpackcompose.util
+
+import com.imsosoft.kotlincountryjetpackcompose.model.CountriesItem
+
+object ExtensionFunc {
+
+    fun CountriesItem.countryParams(name: String, capital: String, region: String, currency: String, flag: String, language: String): CountriesItem {
+        return "country_details_screen/${name}/$capital/$region/$currency/$flag/$language".toCountriesItem()
+    }
+
+    private fun String.toCountriesItem(): CountriesItem {
+        return CountriesItem(
+            name = this.split("/")[1],
+            capital = this.split("/")[2],
+            region = this.split("/")[3],
+            currency = this.split("/")[4],
+            flag = this.split("/")[5],
+            language = this.split("/")[6])
+    }
+}
