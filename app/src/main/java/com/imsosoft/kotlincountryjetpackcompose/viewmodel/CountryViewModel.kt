f5201d4b31c822d0c5e8ece1033dccf0d9100db3
Muarem <s.muarem10@gmail.com>
2024-03-13 20:49:35 +0400

    created country view model

--- app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/viewmodel/CountryViewModel.kt
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/viewmodel/CountryViewModel.kt
@@ -1,4 +1,16 @@
 package com.imsosoft.kotlincountryjetpackcompose.viewmodel
 
-class CountryViewModel {
+import androidx.lifecycle.ViewModel
+import com.imsosoft.kotlincountryjetpackcompose.model.Country
+import com.imsosoft.kotlincountryjetpackcompose.repo.CountryRepo
+import com.imsosoft.kotlincountryjetpackcompose.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CountryViewModel @Inject constructor(private val repo: CountryRepo): ViewModel()
+{
+    suspend fun getCountry(id: String): Resource<Country> {
+        return repo.getCountry(id)
+    }
 }
