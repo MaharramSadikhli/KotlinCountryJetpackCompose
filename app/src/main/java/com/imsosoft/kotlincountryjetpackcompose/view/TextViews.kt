f248cf9d2eafd854b13a18ff08511faea9f8b67a
Muarem <s.muarem10@gmail.com>
2024-03-13 22:49:15 +0400

    created TextViews.kt for composable texts

---
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/view/TextViews.kt
@@ -0,0 +1,32 @@
+package com.imsosoft.kotlincountryjetpackcompose.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CountriesText(text: String) {
+
+    Text(
+        text = text,
+        style = MaterialTheme.typography.displayMedium,
+        modifier = Modifier.padding(2.dp),
+        color = MaterialTheme.colorScheme.secondary
+    )
+
+}
+
+@Composable
+fun CountriesNameText(text: String) {
+
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.padding(2.dp),
+        color = MaterialTheme.colorScheme.primary
+    )
+
+}
