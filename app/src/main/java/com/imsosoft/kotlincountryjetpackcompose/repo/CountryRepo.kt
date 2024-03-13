18aa70fdf1444fa64dcde40206a3092cf18e044e
Muarem <s.muarem10@gmail.com>
2024-03-13 20:06:22 +0400

    created country repo

---
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/repo/CountryRepo.kt
@@ -0,0 +1,33 @@
+package com.imsosoft.kotlincountryjetpackcompose.repo
+
+import com.imsosoft.kotlincountryjetpackcompose.model.Countries
+import com.imsosoft.kotlincountryjetpackcompose.model.Country
+import com.imsosoft.kotlincountryjetpackcompose.service.CountryAPI
+import com.imsosoft.kotlincountryjetpackcompose.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class CountryRepo @Inject constructor(
+    private val api: CountryAPI
+) {
+    suspend fun getCountries(): Resource<Countries> {
+        val response = try {
+            api.getCountries()
+        } catch (e: Exception) {
+            return Resource.Error("Error!")
+        }
+
+        return Resource.Success(response)
+    }
+
+    suspend fun getCountry(): Resource<Country> {
+        val response = try {
+            api.getCountry()
+        } catch (e: Exception) {
+            return Resource.Error("Error!")
+        }
+
+        return Resource.Success(response)
+    }
+}
