6405021d22ec38bd80e7e1eed7653e4b32fb1873
Muarem <s.muarem10@gmail.com>
2024-03-13 19:48:33 +0400

    created countries model

---
+++ app/src/main/java/com/imsosoft/kotlincountryjetpackcompose/model/CountriesItem.kt
@@ -0,0 +1,10 @@
+package com.imsosoft.kotlincountryjetpackcompose.model
+
+data class CountriesItem(
+    val name: String,
+    val capital: String,
+    val region: String,
+    val currency: String,
+    val flag: String,
+    val language: String
+)
